52959af61fe7ae4ff2b79e1284f23fdf530b789d
beatriz.anjos <130382170+Beatriz-dos-Anjos@users.noreply.github.com>
2024-03-10 21:18:14 -0300

    Add files via upload
    teste

---
+++ vma.py
@@ -0,0 +1,10 @@
+import pygame
+
+class VMA:
+    def __init__(self, tela, x, y): #propria instância da classe
+        self.tela = tela
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load('assets/images/objetos_interagiveis/vma.png')
+        self.default_scale = (75, 80)
+        self.image = pygame.transform.scale(self.image, self.default_scale)
