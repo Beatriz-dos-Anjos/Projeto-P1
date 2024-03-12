c6aae0dbfdee989ea6f11e8c1aaff970cbf50180
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2024-03-12 18:40:46 -0300

    adicionando o progresso no arquivo

--- colide.py
+++ colide.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-def combate_obj_pessoas (player, rect, barra_de_vida,hitbox_obj_1,hitbox_obj_2,posicao,posicao_oponente_x,posicao_oponente_y):
+def colisao_obj_pessoas (player, rect, barra_de_vida,hitbox_obj_1,hitbox_obj_2,posicao,posicao_oponente_x,posicao_oponente_y):
     qtd_ganha=5
     quantidade=-15
     colisao_obj_p=False
@@ -8,34 +8,43 @@ def combate_obj_pessoas (player, rect, barra_de_vida,hitbox_obj_1,hitbox_obj_2,p
         #oponente
         if hitbox_obj_1.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y):  #ganha vida
             barra_de_vida.gaining_health(qtd_ganha)
+            colisao_obj_p=True
         if hitbox_obj_2.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y): #perde vida
             barra_de_vida.loose_health(quantidade) 
+            colisao_obj_p=True
+
         
         #ele
 
         if hitbox_obj_1.colliderect(rect.x):  
             barra_de_vida.gaining_health(qtd_ganha)
+            colisao_obj_p=True
              #adiciona vida
         if hitbox_obj_2.colliderect(rect.y):
             barra_de_vida.loose_health(quantidade)
+            colisao_obj_p=True
  
         
     if player==2:
         #oponente
         if hitbox_obj_1.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y):
             barra_de_vida.gaining_health(qtd_ganha)
+            colisao_obj_p=True
 
         if hitbox_obj_2.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y):
             barra_de_vida.loose_health(quantidade)
+            colisao_obj_p=True
         
 
         #ele
 
         if hitbox_obj_1.colliderect(rect.x):  
             barra_de_vida.gaining_health(qtd_ganha)
+            colisao_obj_p=True
 
             #adiciona vida
         if hitbox_obj_2.colliderect(rect.y): 
-            barra_de_vida.gaining_health(quantidade) 
+            barra_de_vida.gaining_health(quantidade)
+            colisao_obj_p=True 
 
         return colisao_obj_p
