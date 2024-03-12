e0fc269d257da7d380bb020805876d71a2739655
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2024-03-12 12:49:34 -0300

    Fazendo , em POO, a colisão dos objetos e pessoas

---
+++ colide.py
@@ -0,0 +1,41 @@
+import pygame
+from pygame.locals import *
+def combate_obj_pessoas (player, rect, barra_de_vida,hitbox_obj_1,hitbox_obj_2,posicao,posicao_oponente_x,posicao_oponente_y):
+    qtd_ganha=5
+    quantidade=-15
+    colisao_obj_p=False
+    if player==1:
+        #oponente
+        if hitbox_obj_1.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y):  #ganha vida
+            barra_de_vida.gaining_health(qtd_ganha)
+        if hitbox_obj_2.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y): #perde vida
+            barra_de_vida.loose_health(quantidade) 
+        
+        #ele
+
+        if hitbox_obj_1.colliderect(rect.x):  
+            barra_de_vida.gaining_health(qtd_ganha)
+             #adiciona vida
+        if hitbox_obj_2.colliderect(rect.y):
+            barra_de_vida.loose_health(quantidade)
+ 
+        
+    if player==2:
+        #oponente
+        if hitbox_obj_1.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y):
+            barra_de_vida.gaining_health(qtd_ganha)
+
+        if hitbox_obj_2.colliderect(posicao_oponente_x) and hitbox_obj_1.colliderect(posicao_oponente_y):
+            barra_de_vida.loose_health(quantidade)
+        
+
+        #ele
+
+        if hitbox_obj_1.colliderect(rect.x):  
+            barra_de_vida.gaining_health(qtd_ganha)
+
+            #adiciona vida
+        if hitbox_obj_2.colliderect(rect.y): 
+            barra_de_vida.gaining_health(quantidade) 
+
+        return colisao_obj_p
