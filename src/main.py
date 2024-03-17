4129213aea8a364f855c86f9c460028f9e2f0576
SérgioL1ra <lirasergio43@gmail.com>
2024-03-17 02:59:07 -0300

    tela inicial e seleçao de personagems feitas falta apenas incrementar com música

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from personagens.taylor import Taylor_fighter
 from personagens.kanye import Kanye_fighter
 from utilities.bars import Barra_de_vida
 from utilities.telainicial import *
-
+from utilities.telainicial import *
 
 
 pygame.init()
@@ -25,9 +25,18 @@ FPS = 45
 # acrescentando o background
 fundo = pygame.image.load('assets/images/background/cenario2.jpeg')
 
+#variaveis seleçao perso
+var_selecao = pygame.image.load('assets/images/background/selecao/antes_de_escolher.png').convert()
+
+fonte = pygame.font.Font(None, 36)
+branco = (255, 255, 255)
+
+personagem_1 = fonte.render("Pressione 1 para selecionar", True, branco)
+personagem_2 = fonte.render("Pressione 2 para selecionar", True, branco)
+
+
 # Função para desenhar o background
 # Essa parte do código é essencial para ser chamada no loop, pois sem ela os personagens não se movimentam, a imagem deles é desenhada
-
 
 def bg():
     escala = pygame.transform.scale(fundo, (largura, altura))
@@ -35,25 +44,34 @@ def bg():
 
 
 # Criando os personagens
-Taylor_Swift = Taylor_fighter(1, 200, 310)
-Kanye_West = Kanye_fighter(2, 700, 310)
+#Taylor_Swift = Taylor_fighter(1, 200, 310)
+#Kanye_West = Kanye_fighter(2, 700, 310)
 
+#remover linha pois vai ser definido na tela de selaçao
 
 # criando as barras de vida e as barras de especial dos personagens
-Taylor_Swift_Bars = Barra_de_vida(1, tela)
-Kanye_West_Bars = Barra_de_vida(2, tela)
+#Taylor_Swift_Bars = Barra_de_vida(1, tela)
+#Kanye_West_Bars = Barra_de_vida(2, tela)
 
+#remover linha pois vai ser definido na tela de selaçao
+    
 #
 localizacao_kanye_x, localizacao_kanye_y = 0, 0
 localizacao_taylor_x, localizacao_taylor_y = 0, 0
 
+#sprites da tela inicial
 logo = Logo()
 start = Start()
 sprites.add(logo)
 sprites.add(start)
 
+#tupla para auxilinar na seleçao de personagem
+personagems = ()
 
+
+#variavel para controlar as cenas
 cena = "menu"
+
 # loop
 while True:
     relogio.tick(FPS)
@@ -64,7 +82,7 @@ while True:
                 exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    cena = 'jogo'
+                    cena = 'selecao'
         
         tela.blit(tela_inicial, (0,0))
 
@@ -72,7 +90,57 @@ while True:
         sprites.draw(tela)
         sprites.update()
 
- 
+    elif cena == "selecao":
+
+        tela_selecao = pygame.transform.scale(var_selecao, (largura,altura))
+
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    if 'Kanye' not in personagems:
+                        var_selecao = pygame.image.load('assets/images/background/selecao/p1_kanye.png').convert()
+                        tela_selecao = pygame.transform.scale(var_selecao, (largura,altura))
+                        Kanye_West = Kanye_fighter(1, 700, 310)
+                        Kanye_West_Bars = Barra_de_vida(1, tela)
+                        list_personagem = list(personagems)
+                        list_personagem.append('Kanye')
+                        personagems = tuple(list_personagem)
+
+                elif event.key == pygame.K_2:
+                    if 'Taylor' not in personagems:
+                        var_selecao = pygame.image.load('assets/images/background/selecao/p2_taylor.png').convert()
+                        tela_selecao = pygame.transform.scale(var_selecao, (largura,altura))
+                        Taylor_Swift = Taylor_fighter(2, 200, 310)
+                        Taylor_Swift_Bars = Barra_de_vida(2, tela)
+                        list_personagem = list(personagems)
+                        list_personagem.append('Taylor')
+                        personagems = tuple(list_personagem)
+
+        if len(personagems) == 2:
+            var_selecao = pygame.image.load('assets/images/background/selecao/escolhidos.png').convert()
+            tela_selecao = pygame.transform.scale(var_selecao, (largura,altura))
+            sprites.remove(logo)
+            sprites.draw(tela_selecao)
+            sprites.update()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    cena = 'jogo'
+
+                
+        
+                    
+        tela.blit(tela_selecao, (0,0))
+        if 'Kanye' not in personagems:
+            tela.blit(personagem_1, [50, 500])
+        if 'Taylor' not in personagems:
+            tela.blit(personagem_2, [650, 500])
+
     elif cena == "jogo":
         bg()
         # Interação de Combate
