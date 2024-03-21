85ca1a945c6f16a7378dc6e91382730a62fac2d7
antoniorpcarolino@gmail.com <antoniorpcarolino@gmail.com>
2024-03-20 21:25:15 -0300

    sprites  inserction

--- src/personagens/taylor.py
+++ src/personagens/taylor.py
@@ -4,6 +4,9 @@ from utilities.fight_commands import combate
 from utilities.movimentacao import move
 from personagens.animation_taylor import idle, walk, punch, kick, derrota
 
+largura = 1000
+altura = 600
+
 
 class Taylor_fighter():
     def __init__(self, player, x, y):
@@ -20,48 +23,47 @@ class Taylor_fighter():
         self.last_time_animation = 0
         self.speed = 0
         self.attacking = False
-        self.block = False # flag para travar execução do player ao executar qualquer movimento de combate
+        # flag para travar execução do player ao executar qualquer movimento de combate
+        self.block = False
         self.ground = True
         self.dead = False
         self.facing_left = True if self.player == 2 else False
 
-
     def combate(self, surface, posicao_oponente_x, posicao_oponente_y, barra_de_vida_oponente, barra_de_vida_player, defendendo_ou_nao):
         # Temporizador para o personagem só poder atacar quando ele não estiver executando outro movimento de ataque
-        if self.is_performming_attack == False and self.attacking == False: 
-            self.attacking, self.attacking_type, defendendo_ou_nao_taylor = combate(self.rect, self.player, surface, posicao_oponente_x, posicao_oponente_y, barra_de_vida_oponente, barra_de_vida_player, defendendo_ou_nao) 
-            
+        if self.is_performming_attack == False and self.attacking == False:
+            self.attacking, self.attacking_type, defendendo_ou_nao_taylor = combate(
+                self.rect, self.player, surface, posicao_oponente_x, posicao_oponente_y, barra_de_vida_oponente, barra_de_vida_player, defendendo_ou_nao)
 
     def move(self, largura, altura, inimigo):
-        self.ground, self.speed, self.is_moving = move(self.rect, self.player, self.ground, self.speed, largura, altura, inimigo, self.is_moving, self.block)
-
+        self.ground, self.speed, self.is_moving = move(
+            self.rect, self.player, self.ground, self.speed, largura, altura, inimigo, self.is_moving, self.block)
 
     def return_x_y(self):
         return (self.rect.x, self.rect.y)
 
-
     def return_rect(self):
         return self.rect
-
 
     def draw(self, surface):
 
         self.atualizar_animacao()
-        surface.blit(self.animations[self.current_animation][self.current_animation_frame], self.rect.topleft)
+        surface.blit(self.animations[self.current_animation]
+                     [self.current_animation_frame], self.rect.topleft)
 
-        #pygame.draw.rect(surface, (255, 0, 0), self.rect)
-
+        # pygame.draw.rect(surface, (255, 0, 0), self.rect)
 
     def atualizar_animacao(self):
 
         # feita para fazer com que os frames passem após um cooldown de tempo entre cada uma das animações
         if pygame.time.get_ticks() - self.last_time_animation >= self.time_animation:
 
-            self.last_time_animation = pygame.time.get_ticks() # guarda o momento do último frame
-            
+            # guarda o momento do último frame
+            self.last_time_animation = pygame.time.get_ticks()
+
             # Troca para a animação de soco
             if self.attacking_type == 1 and self.is_performming_attack == False and self.dead == False:
-                self.current_animation = 2 # punch
+                self.current_animation = 2  # punch
                 self.current_animation_frame = 0
                 self.is_moving = False
                 self.is_performming_attack = True
@@ -69,24 +71,24 @@ class Taylor_fighter():
                 # execução será travada
 
             if self.attacking_type == 2 and self.is_performming_attack == False and self.dead == False:
-                self.current_animation = 4 # kick
+                self.current_animation = 4  # kick
                 self.current_animation_frame = 0
                 self.is_moving = False
                 self.is_performming_attack = True
                 self.block = True
                 # execução será travada
 
-            # se o botão de andar for pressionado a animação passa a ser a de 
+            # se o botão de andar for pressionado a animação passa a ser a de
             elif self.is_moving == True and self.is_performming_attack == False and self.current_animation != 1 and self.dead == False:
-                self.current_animation = 1 # walk
+                self.current_animation = 1  # walk
                 self.current_animation_frame = 0
                 self.is_moving = True
-            
+
             elif self.is_moving == False and self.is_performming_attack == False and self.current_animation != 0 and self.dead == False:
-                self.current_animation = 0 #idle
+                self.current_animation = 0  # idle
                 self.current_animation_frame = 0
 
-            # confere se a última animação acabou 
+            # confere se a última animação acabou
             if self.current_animation_frame >= len(self.animations[self.current_animation]) - 1:
 
                 # confere se o botão de andar continua sendo pressionado:: se continuar --> o movimento continua
@@ -98,7 +100,7 @@ class Taylor_fighter():
                 elif self.dead == False:
                     self.current_animation = 0
                     self.current_animation_frame = 0
-                
+
                 elif self.dead == True:
                     self.current_animation = 4
                     self.current_animation_frame = 0
@@ -109,30 +111,29 @@ class Taylor_fighter():
                 self.block = False
                 # execução vai destravar aqui
 
-            # se não tiver acabado passa para o próximo fram da animação 
+            # se não tiver acabado passa para o próximo fram da animação
             else:
 
                 # confere se o botão de andar parou de ser pressionado:: se parar --> o movimento de andar para
                 if self.is_moving == False and self.is_performming_attack == False and self.current_animation != 0 and self.dead == False:
                     self.current_animation = 0
                     self.current_animation_frame = 0
-                
+
                 # Pula para o próximo quadro de frame da animação
                 else:
                     self.current_animation_frame = self.current_animation_frame + 1
-    
 
-    def die(self): # animação usada quando o personagem perde
+    def die(self):  # animação usada quando o personagem perde
         self.rect.y = 500
         self.dead = True
         self.current_animation = 4
         self.current_animation_frame = 0
-        
+
     # vai passar uma animação que o personagem perde, ele vai animar e mover o personagem correndo para fora da tela
     # vai comunicar assim:
     # No main vai puxar o get_life()
     # O código de luta acontece enquanto get_life() de ambos os jogadores forem diferentes de 0
-    # Quando bater 0 quebra o laço de repetição e manda a animação de perder 
+    # Quando bater 0 quebra o laço de repetição e manda a animação de perder
     # (essa animação aqui) --> personagem correndo para fora da tela
     # depois o outro comemora de alguma forma
     # mostra uma tela de configuração porque acabou o jogo
--- src/utilities/fight_commands.py
+++ src/utilities/fight_commands.py
@@ -1,110 +1,132 @@
 import pygame
 from pygame.locals import *
-from utilities.colisoes import colisao # Não está pegando sem o utilities
+from utilities.colisoes import colisao  # Não está pegando sem o utilities
+
+pygame.init()
+musica_de_soco = pygame.mixer.Sound(
+    'assets/images/sounds/punch-140236.mp3')
 
 
 def combate(rect, player, surface, posicao_oponente_x, posicao_oponente_y, barra_de_vida_oponente, barra_de_vida_player, inimigo_esta_defendendo):
-        attacking_damage = 0
-        attacked_or_not = False
-        tipo_de_ataque = None
-        defendendo_ou_nao = False
-        
-        keys = pygame.key.get_pressed()
+    attacking_damage = 0
+    attacked_or_not = False
+    tipo_de_ataque = None
+    defendendo_ou_nao = False
 
-        if player == 1:  # COMANDOS DE ATAQUE PARA O PLAYER 1
+    keys = pygame.key.get_pressed()
 
-            # ATAQUE DE SOCO
-            if keys[pygame.K_e]:
+    if player == 1:  # COMANDOS DE ATAQUE PARA O PLAYER 1
 
-                attacking_damage = 9 if inimigo_esta_defendendo == False else 1 # Quantidade de dano
-                attacked_or_not = True # variável para se ele atacou ou não
+        # ATAQUE DE SOCO
+        if keys[pygame.K_e]:
 
-                area_de_colisao = pygame.Rect(rect.x + 165, rect.y, 80, 100)  #área de colisão
+            attacking_damage = 9 if inimigo_esta_defendendo == False else 1  # Quantidade de dano
+            attacked_or_not = True  # variável para se ele atacou ou não
 
-                pygame.draw.rect(surface, (0, 255, 0), area_de_colisao) # desenha a hitbox do ataque
-                posicao = pygame.Rect(posicao_oponente_x, posicao_oponente_y, 80, 180)  # desenha a hit box do personagem inimigo 
-                tipo_de_ataque = 1 # vai servir para a animação, 1 == Soco
+            area_de_colisao = pygame.Rect(
+                rect.x + 165, rect.y, 80, 100)  # área de colisão
 
-                colisao(area_de_colisao, posicao, attacking_damage, barra_de_vida_oponente)  # invoca a função para verificar se houve colisão entre o golpe e o inimigo 
+            # desenha a hitbox do ataque
+            pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
+            # desenha a hit box do personagem inimigo
+            posicao = pygame.Rect(posicao_oponente_x,
+                                  posicao_oponente_y, 80, 180)
+            tipo_de_ataque = 1  # vai servir para a animação, 1 == Soco
 
-            # ATAQUE DE CHUTE
-            if keys[pygame.K_r]:
+            # invoca a função para verificar se houve colisão entre o golpe e o inimigo
+            colisao(area_de_colisao, posicao,
+                    attacking_damage, barra_de_vida_oponente)
 
-                attacking_damage = 13 if inimigo_esta_defendendo == False else 3
-                attacked_or_not = True
-                
-                area_de_colisao = pygame.Rect(rect.x + 140, rect.y + 60, 80, 150)
+            musica_de_soco.play()
+        # ATAQUE DE CHUTE
+        if keys[pygame.K_r]:
 
-                pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
-                posicao = pygame.Rect(posicao_oponente_x, posicao_oponente_y, 80, 180)
-                tipo_de_ataque = 2 # vai servir para a animação, 2 == chute
+            attacking_damage = 13 if inimigo_esta_defendendo == False else 3
+            attacked_or_not = True
 
-                colisao(area_de_colisao, posicao, attacking_damage, barra_de_vida_oponente)
+            area_de_colisao = pygame.Rect(rect.x + 140, rect.y + 60, 80, 150)
 
-            # ATAQUE ESPECIAL
-            if keys[pygame.K_t] and barra_de_vida_player.can_use_special() == True:
+            pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
+            posicao = pygame.Rect(posicao_oponente_x,
+                                  posicao_oponente_y, 80, 180)
+            tipo_de_ataque = 2  # vai servir para a animação, 2 == chute
 
-                attacking_damage = 100
-                barra_de_vida_player.loose_special()
-                attacked_or_not = True
-                
-                area_de_colisao = pygame.Rect(rect.x + 80, rect.y, 80, 180)
+            colisao(area_de_colisao, posicao,
+                    attacking_damage, barra_de_vida_oponente)
 
-                pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
-                posicao = pygame.Rect(posicao_oponente_x, posicao_oponente_y, 80, 180)
-                tipo_de_ataque = 3 # vai servir para a animação, 3 == especial
+            musica_de_soco.play()
+        # ATAQUE ESPECIAL
+        if keys[pygame.K_t] and barra_de_vida_player.can_use_special() == True:
 
-                colisao(area_de_colisao, posicao, attacking_damage, barra_de_vida_oponente)
+            attacking_damage = 100
+            barra_de_vida_player.loose_special()
+            attacked_or_not = True
 
+            area_de_colisao = pygame.Rect(rect.x + 80, rect.y, 80, 180)
 
-            if keys[pygame.K_f]:
-                defendendo_ou_nao = True
+            pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
+            posicao = pygame.Rect(posicao_oponente_x,
+                                  posicao_oponente_y, 80, 180)
+            tipo_de_ataque = 3  # vai servir para a animação, 3 == especial
 
+            colisao(area_de_colisao, posicao,
+                    attacking_damage, barra_de_vida_oponente)
 
-        elif player == 2:  # COMANDOS DE ATAQUE PARA O PLAYER 2
+        if keys[pygame.K_f]:
+            defendendo_ou_nao = True
 
-            # ATAQUE DE SOCO
-            if keys[pygame.K_b]:
+    elif player == 2:  # COMANDOS DE ATAQUE PARA O PLAYER 2
 
-                attacking_damage = 5 if inimigo_esta_defendendo == False else 1 
-                attacked_or_not = True
+        # ATAQUE DE SOCO
+        if keys[pygame.K_b]:
 
-                area_de_colisao = pygame.Rect(rect.x, rect.y, -80, 100)  #área de colisão
+            attacking_damage = 5 if inimigo_esta_defendendo == False else 1
+            attacked_or_not = True
 
-                pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
-                posicao = pygame.Rect(posicao_oponente_x, posicao_oponente_y, 80, 180)
-                tipo_de_ataque = 1 # vai servir para a animação, 1 == Soco
+            area_de_colisao = pygame.Rect(
+                rect.x, rect.y, -80, 100)  # área de colisão
 
-                colisao(area_de_colisao, posicao, attacking_damage, barra_de_vida_oponente)
+            pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
+            posicao = pygame.Rect(posicao_oponente_x,
+                                  posicao_oponente_y, 80, 180)
+            tipo_de_ataque = 1  # vai servir para a animação, 1 == Soco
 
-            # ATAQUE DE CHUTE
-            if keys[pygame.K_n]:
+            colisao(area_de_colisao, posicao,
+                    attacking_damage, barra_de_vida_oponente)
 
-                attacking_damage = 7 if inimigo_esta_defendendo == False else 3
-                attacked_or_not = True
-                
-                area_de_colisao = pygame.Rect(rect.x - 140, rect.y + 60, 80, 150)
+            musica_de_soco.play()
+        # ATAQUE DE CHUTE
+        if keys[pygame.K_n]:
 
-                pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
-                posicao = pygame.Rect(posicao_oponente_x, posicao_oponente_y, 80, 180)
-                tipo_de_ataque = 2 # vai servir para a animação, 2 == chute
+            attacking_damage = 7 if inimigo_esta_defendendo == False else 3
+            attacked_or_not = True
 
-                colisao(area_de_colisao, posicao, attacking_damage, barra_de_vida_oponente)
+            area_de_colisao = pygame.Rect(rect.x - 140, rect.y + 60, 80, 150)
 
-            # ATAQUE ESPECIAL
-            if keys[pygame.K_m] and barra_de_vida_player.can_use_special() == True:
+            pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
+            posicao = pygame.Rect(posicao_oponente_x,
+                                  posicao_oponente_y, 80, 180)
+            tipo_de_ataque = 2  # vai servir para a animação, 2 == chute
 
-                attacking_damage = 33 
-                barra_de_vida_player.loose_special()
-                attacked_or_not = True
-                
-                area_de_colisao = pygame.Rect(rect.x - 80, rect.y, 80, 180)
+            colisao(area_de_colisao, posicao,
+                    attacking_damage, barra_de_vida_oponente)
 
-                pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
-                posicao = pygame.Rect(posicao_oponente_x, posicao_oponente_y, 80, 180)
-                tipo_de_ataque = 3 # vai servir para a animação, 3 == especial
+            musica_de_soco.play()
+        # ATAQUE ESPECIAL
+        if keys[pygame.K_m] and barra_de_vida_player.can_use_special() == True:
 
-                colisao(area_de_colisao, posicao, attacking_damage, barra_de_vida_oponente)
+            attacking_damage = 33
+            barra_de_vida_player.loose_special()
+            attacked_or_not = True
 
+            area_de_colisao = pygame.Rect(rect.x - 80, rect.y, 80, 180)
 
-        return attacked_or_not, tipo_de_ataque, defendendo_ou_nao
+            pygame.draw.rect(surface, (0, 255, 0), area_de_colisao)
+            posicao = pygame.Rect(posicao_oponente_x,
+                                  posicao_oponente_y, 80, 180)
+            tipo_de_ataque = 3  # vai servir para a animação, 3 == especial
+
+            colisao(area_de_colisao, posicao,
+                    attacking_damage, barra_de_vida_oponente)
+
+    return attacked_or_not, tipo_de_ataque, defendendo_ou_nao
